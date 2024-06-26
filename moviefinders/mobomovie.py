0f05c0cb4084fce4ca22d9d046d01337201d2823
mahdi-ramezanpoor <20mahadi.r@proton.me>
2024-06-26 23:58:54 +0330

    base code for tele and finders

---
+++ moviefinders/mobomovie.py
@@ -0,0 +1,106 @@
+import json
+import re
+from os import link
+
+import requests
+from bs4 import BeautifulSoup as bs
+from bs4.element import NavigableString, Tag
+
+proxies = {
+    "http": "http://127.0.0.1:2081",
+    "https": "http://127.0.0.1:2081",
+}
+
+aderess = "https://mobomovies1.site"
+
+
+def process_series_data(data):
+    seasons = {}
+    for season_key, season_data in data.items():
+        season_num = int(season_key)
+        if season_num not in seasons:
+            seasons[season_num] = {}
+
+        for key, value in season_data.items():
+            info = value['info']
+            urls = value['urls']
+
+            type_key = info['type']
+            resolution_quality_codec = f"{info['resolution']} {
+                info['quality']} {info['codec']} {info['encode']}"
+
+            if type_key not in seasons[season_num]:
+                seasons[season_num][type_key] = {}
+
+            if resolution_quality_codec not in seasons[season_num][type_key]:
+                seasons[season_num][type_key][resolution_quality_codec] = {}
+
+            for url in urls:
+                episode_title = url['title']
+                episode_url = url['file']
+                seasons[season_num][type_key][resolution_quality_codec][episode_title] = episode_url
+
+    season_count = len(seasons)
+    return season_count, seasons
+
+
+def process_movie_data(data):
+    movie = {}
+    for file_type, files in data.items():
+        file_info = {}
+        for file_details in files:
+            resolution = file_details['url_resolution']
+            quality = file_details['url_quality']
+            codec_encode = f"{
+                file_details['url_codec']}/{file_details['url_encode']}"
+            file_url = file_details['url_file']
+            file_info[resolution+" "+quality+" "+codec_encode] = file_url
+        movie[file_type] = file_info
+    return movie
+
+
+def find_links(post_id, type):
+    data = {"post_id": post_id}
+    request = requests.post(aderess+"/api/get-urls",
+                            data=data)
+    if type == "series":
+        season, links = process_series_data(request.json())
+        return season, links
+    else:
+        links = process_movie_data(request.json())
+        return "movie", links
+
+
+def find_movie(name, imdbid):
+    request = requests.get(f'{aderess}/s/{name}')
+    soup = bs(request.content, 'html.parser')
+    posts = soup.find_all('article', class_='post-item')
+    pagelink = list()
+    for post in posts:
+
+        img_tags = post.find_all('img', class_='thumbnail')
+        for img in img_tags:
+            if imdbid in img['src']:
+                pagelinks = post.find('a', class_='mdb')
+                if pagelinks:
+                    pagelink.append(aderess+pagelinks['href'])
+    data = list()
+    li_tag=None
+    if pagelink:
+        request = requests.get(pagelink[0])
+        soup = bs(request.content, 'html.parser')
+        li_tag = soup.find('li', class_='tab-item',attrs={"data-target": "dls"})
+    else:
+        return None
+    if li_tag:
+        onclick_content = re.search(
+            r"loadUrls\((\d+),\s*'(\w+)'\)", li_tag['onclick'])
+        if onclick_content:
+            number, text = onclick_content.groups()
+            data.append(number)
+            data.append(text)
+    return data
+
+
+print(find_movie("WALL·E", "tt0910970"))
+#print(find_links("350","series"))
---
+++ telegrambot.py
@@ -0,0 +1,70 @@
+import asyncio
+import logging
+import sys
+from os import getenv
+import re
+from aiogram import Bot, Dispatcher, html 
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+import pasegs
+import moviefinders.almasmovie
+import moviefinders.mobomovie
+# Bot token can be obtained via https://t.me/BotFather
+TOKEN = "6664665455:AAHoJRgMdNLz9aYbC2elfRHjgUlNpB7szh8"
+# All handlers should be attached to the Router (or Dispatcher)
+
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"سلام, {html.bold(message.from_user.full_name)}!\n" + pasegs.start_message)
+
+
+@dp.message()
+async def get_name_movie(message: Message) -> None:
+    # handler @imdbbot message
+    try:
+        if message.via_bot and message.via_bot.username == "imdbot":
+            pattern = r'tt\d+'
+            match = re.search(pattern,  message.entities[1].url)
+            if match:
+                imdb_id = match.group()
+            movie_name = message.text
+            try:
+                almasmovie_page = moviefinders.almasmovie.find_movie(imdb_id)
+                mobomovie_page = moviefinders.mobomovie.find_movie(movie_name,imdb_id)
+                if almasmovie_page[0] == None and mobomovie_page==None:
+                    await message.answer(pasegs.not_fouand)
+                else:
+                    almaslinks = moviefinders.almasmovie.find_links(almasmovie_page[0],almasmovie_page[1])
+                    mobolinks =  moviefinders.mobomovie.find_links(mobomovie_page[0],mobomovie_page[1])
+                    alllinks = mobolinks | almaslinks
+                    builder = InlineKeyboardBuilder()
+                    for index in alllinks.items():
+                        builder.button(text=f"Set {index[0]}", callback_data=f"{index[0]}")
+                    await message.answer(pasegs.finded,reply_markup=builder.as_markup())
+            except TypeError:
+                ...
+    except TypeError:
+        await message.answer(pasegs.format_not_suport)
+
+proxies = {
+    "http": "http://127.0.0.1:2081",
+    "https": "http://127.0.0.1:2081",
+}
+
+async def main() -> None:
+    # Initialize Bot instance with default bot properties which will be passed to all API calls
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(
+        parse_mode=ParseMode.HTML),proxies=proxies)
+
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
