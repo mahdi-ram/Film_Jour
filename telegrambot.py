aed5b6ec6de8993378e35c412a9ece58d4b8b89b
mahdi-ramezanpoor <20mahadi.r@proton.me>
2024-06-27 17:16:10 +0330

    showe movie buten

--- telegrambot.py
+++ telegrambot.py
@@ -6,16 +6,21 @@ from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
-from aiogram.types import Message ,InlineKeyboardMarkup ,InlineKeyboardButton
+from aiogram.types import Message ,InlineKeyboardMarkup ,InlineKeyboardButton 
 import pasegs
 import moviefinders.almasmovie
 import moviefinders.mobomovie
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 # Bot token can be obtained via https://t.me/BotFather
 TOKEN = "6664665455:AAHoJRgMdNLz9aYbC2elfRHjgUlNpB7szh8"
 # All handlers should be attached to the Router (or Dispatcher)
 
 dp = Dispatcher()
-def remove_username(text: str) -> str:
+
+def clean_text(text: str) -> str:
+    # حذف کاراکترهای نامرئی
+    text = re.sub(r'[^\x00-\x7F]+', ' ', text)
+    # حذف یوزرنیم تلگرام
     pattern = r'@TgISTRASH$'
     result = re.sub(pattern, '', text).strip()
     return result
@@ -33,12 +38,10 @@ async def get_name_movie(message: Message) -> None:
             match = re.search(pattern,  message.entities[1].url)
             if match:
                 imdb_id = match.group()
-            movie_name = remove_username(message.text)
+            movie_name = clean_text(message.text)
             try:
                 almasmovie_page = moviefinders.almasmovie.find_movie(imdb_id)
-                mobomovie_page = moviefinders.mobomovie.find_movie(movie_name,imdb_id)
-                await message.answer(almasmovie_page)
-                await message.answer(mobomovie_page)
+                mobomovie_page = moviefinders.mobomovie.find_movie(movie_name.strip(),imdb_id)
                 if almasmovie_page[0] is None and mobomovie_page is None:
                     await message.answer(pasegs.not_fouand)
                 elif  mobomovie_page[0] is None:
@@ -58,15 +61,14 @@ async def get_name_movie(message: Message) -> None:
                         keyboard.add(button)
                     await message.answer(pasegs.finded,reply_markup=keyboard)
                 else:
-                    print("12222222222")
                     almaslinks = moviefinders.almasmovie.find_links(almasmovie_page[0],almasmovie_page[1])
                     mobolinks =  moviefinders.mobomovie.find_links(mobomovie_page[0],mobomovie_page[1])
-                    alllinks = almaslinks | mobolinks
-                    keyboard = InlineKeyboardMarkup()
-                    for index in alllinks.items():
-                        button = InlineKeyboardButton(index, callback_data='button_clicked')
-                        keyboard.add(button)
-                    await message.answer(pasegs.finded,reply_markup=keyboard)
+                    alllinks = almaslinks[1] | mobolinks[1]
+                    builder = InlineKeyboardBuilder()
+                    for index ,value in alllinks.items():
+                        builder.button(text=f"{index}", callback_data="set:{index}")
+                    builder.adjust(2,3)
+                    await message.answer(pasegs.finded,reply_markup=builder.as_markup())
             except TypeError:
                 ...
     except TypeError:
